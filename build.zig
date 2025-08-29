69e2cbd88aa0a2abcf4e33b4dfd8ee638047e190
okonomipizza <140386510+okonomipizza@users.noreply.github.com>
2025-08-29 21:01:41 +0900

    Fix type

--- build.zig
+++ build.zig
@@ -5,7 +5,7 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     _ = b.addModule("jsonpico", .{
-        .root_source_file = b.path("src/root.zig");
+        .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
